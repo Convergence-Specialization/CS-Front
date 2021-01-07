a62cab055fdc4ca84234135b72956b430b097ed6
cassfresh <62606632+cassfresh@users.noreply.github.com>
2021-01-07 23:52:14 +0900

    이거로 받아주세요

--- src/App.js
+++ src/App.js
@@ -4,24 +4,17 @@ import Home from "./Home";
 import { Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Proto from "./Proto";
+import YoungHyun from "./Test_Branches/YoungHyun";
 
 function App() {
   return (
     <div>
-      <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
-          <Link to="/about">어바웃</Link>
-        </li>
-        <li>
-          <Link to="/proto">Prototype</Link>
-        </li>
-      </ul>
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} />
       <Route path="/proto" component={Proto} />
+      <Route path="/test">
+        <YoungHyun />
+      </Route>
     </div>
   );
 }
--- src/Test_Branches/YoungHyun.js
+++ src/Test_Branches/YoungHyun.js
@@ -1,1 +1,6 @@
+import React from "react";
 // 나는 영현
+const YoungHyun = () => {
+  return <div>나는 영현</div>;
+};
+export default YoungHyun;

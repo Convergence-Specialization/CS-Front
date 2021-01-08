0474f5ada296e6abc19854678298ee1b09996dfb
cassfresh <62606632+cassfresh@users.noreply.github.com>
2021-01-08 23:20:34 +0900

    준하랑 css 공부

--- src/Test_Branches/JunHa.js
+++ src/Test_Branches/JunHa.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { keyframes } from "styled-components";
 
 const Con = styled.div`
@@ -24,10 +24,25 @@ const AnnounceCon = styled.div`
   margin: 40px auto;
   padding: 15px;
   width: 95%;
-  height: 25px;
   background-color: #e3fdfd;
   box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
     0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const TextSmallBox = styled.div`
+  border: 1px solid black;
+  text-align: center;
+  padding: 10px 20px;
+  border-radius: 30px;
+  background-color: #f88f01;
+  box-shadow: 0 6px 12px -1px rgba(0, 0, 0, 0.12);
+  transition: all 0.2s ease;
+  &:hover {
+    margin-top: -10px;
+    cursor: pointer;
+  }
 `;
 const MoveCon = styled.div`
   font-weight: bold;
@@ -37,11 +52,12 @@ const MoveCon = styled.div`
   align-items: center;
   justify-content: space-between;
 `;
+const SidebarContainer = styled.div`
+  position: fixed;
+  margin-left: -150px;
+`;
 const MoveSmallCon = styled.div`
-  width: 20%;
-  position: relative;
-        left: 10px;
-  height: 500px;
+  /* width: 20%; */
   padding: 15px;
   background-color: #e3fdfd;
   box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
@@ -51,12 +67,12 @@ const MoveSmallCon = styled.div`
     margin: 10px 0;
   }
 `;
+const LinkButton = styled.div`
+  border: 1px solid black;
+  background-color: white;
+  margin: 10px 0;
+`;
 const GoveSmallCon = styled.div`
-  width: 70%;
-  height: 50px;
-  position: relative;
-  right: 10px;
-  bottom: 224px;
   padding: 15px;
   background-color: #e3fdfd;
   box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
@@ -67,12 +83,12 @@ const GoveSmallCon = styled.div`
   }
 `;
 const QoveSmallCon = styled.div`
-  width: 20%;
-  height: 150px;
-  position: relative;
-        left: 10px;
-        bottom: 205px;
+  margin-top: 20px;
   padding: 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
   background-color: #e3fdfd;
   box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
     0 2px 4px -1px rgba(0, 0, 0, 0.06);
@@ -80,13 +96,13 @@ const QoveSmallCon = styled.div`
     width: 100%;
     margin: 10px 0;
   }
+`;
+const QoveText = styled.div`
+  margin: 20px 0;
 `;
 const BoveSmallCon = styled.div`
   width: 70%;
-  height: 600px;
-  position: relative;
-        right: 10px;
-        bottom: 430px;
+  margin: 10px auto;
   padding: 15px;
   background-color: #e3fdfd;
   box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
@@ -96,6 +112,12 @@ const BoveSmallCon = styled.div`
     margin: 10px 0;
   }
 `;
+const ChildWrapper = styled.div`
+  display: flex;
+  background-color: pink;
+  margin: 10px 0;
+`;
+const ChildText = styled.div``;
 
 const FreeCon = styled.div`
   font-weight: bold;
@@ -118,11 +140,6 @@ const FreeSmallCon = styled.div`
     margin: 10px 0;
   }
 `;
-const TextSmallBox = styled.div`
-  width: 100%;
-  height: 20%;
-  /* border-bottom: 1px solid black; */
-`;
 const ImgCon = styled.div`
   width: 100%;
   display: flex;
@@ -133,27 +150,142 @@ const ImgBox = styled.img`
   width: 95%;
 `;
 
+const DUMMY_list = [
+  {
+    title: "준하 안녕",
+    author: "준하",
+    time: "2020/12/31",
+  },
+  {
+    title: "준하 안녕",
+    author: "준하",
+    time: "2020/12/31",
+  },
+  {
+    title: "준하 안녕",
+    author: "준하",
+    time: "2020/12/31",
+  },
+  {
+    title: "준하 안녕",
+    author: "준하",
+    time: "2020/12/31",
+  },
+  {
+    title: "준하 안녕",
+    author: "준하",
+    time: "2020/12/31",
+  },
+  {
+    title: "준하 안녕",
+    author: "준하",
+    time: "2020/12/31",
+  },
+  {
+    title: "준하 안녕",
+    author: "준하",
+    time: "2020/12/31",
+  },
+  {
+    title: "준하 안녕",
+    author: "준하",
+    time: "2020/12/31",
+  },
+  {
+    title: "준하 안녕",
+    author: "준하",
+    time: "2020/12/31",
+  },
+  {
+    title: "준하 안녕",
+    author: "준하",
+    time: "2020/12/31",
+  },
+  {
+    title: "준하 안녕",
+    author: "준하",
+    time: "2020/12/31",
+  },
+  {
+    title: "준하 안녕",
+    author: "준하",
+    time: "2020/12/31",
+  },
+  {
+    title: "준하 안녕",
+    author: "준하",
+    time: "2020/12/31",
+  },
+  {
+    title: "준하 안녕",
+    author: "준하",
+    time: "2020/12/31",
+  },
+  {
+    title: "준하 안녕",
+    author: "준하",
+    time: "2020/12/31",
+  },
+];
+
 const Proto = () => {
+  const [number, setNumber] = useState(0);
   return (
     <Con>
       <NavTemp />
       <Wrap>
+        {/* 상단바 구역 */}
         <AnnounceCon>
-          <TextSmallBox>공지사항 | 게시판 |&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;...&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;|마이페이지</TextSmallBox>
+          <TextSmallBox
+            onClick={() => {
+              window.location.href = "/About";
+            }}>
+            공지사항
+          </TextSmallBox>
+          <TextSmallBox
+            onClick={() => {
+              setNumber(number + 1);
+            }}>
+            게시판
+          </TextSmallBox>
+          <TextSmallBox>{number}</TextSmallBox>
+          <TextSmallBox>마이페이지</TextSmallBox>
+          <TextSmallBox>마이페이지</TextSmallBox>
         </AnnounceCon>
+        {/* 밑 구역 */}
         <MoveCon>
-          <MoveSmallCon>게시판<br/><br/><br/>-자유게시판<br/><br/><br/>-21학번 게시판<br/><br/><br/>-20학번 게시판<br/><br/><br/>-등등...?</MoveSmallCon>
-          <GoveSmallCon>20학번 게시판</GoveSmallCon>
-          <QoveSmallCon>슈케치&emsp;&emsp;씨아이<br/><br/>융특&emsp;&emsp;프로젝트</QoveSmallCon>
-          <BoveSmallCon>날짜&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;제목&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;작성자
-            <br/><br/>2021/1/8&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;모두 화이팅 하십쇼&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;김준하
-            <br/><br/>2021/1/8&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;컴학의 미래 지여누&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;김준하
-            <br/><br/>2021/1/8&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;컴학의 천재 아는소&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;김준하
-            </BoveSmallCon>
+          {/* 사이드 바 구역 */}
+          <SidebarContainer>
+            <MoveSmallCon>
+              <LinkButton>20학번 게시판</LinkButton>
+              <LinkButton>20학번 게시판</LinkButton>
+              <LinkButton>20학번 게시판</LinkButton>
+              <LinkButton>20학번 게시판</LinkButton>
+            </MoveSmallCon>
+            <QoveSmallCon>
+              <QoveText>슈케치</QoveText>
+              <QoveText>슈케치</QoveText>
+              <QoveText>슈케치</QoveText>
+              <QoveText>슈케치</QoveText>
+            </QoveSmallCon>
+          </SidebarContainer>
+          {/* 메인 구역 */}
+          <BoveSmallCon>
+            <ChildWrapper>
+              <ChildText style={{ width: "50%" }}>제목</ChildText>
+              <ChildText style={{ width: "25%" }}>작성자</ChildText>
+              <ChildText style={{ width: "25%" }}>작성 시간</ChildText>
+            </ChildWrapper>
+            {DUMMY_list.map((item, idx) => (
+              <ChildWrapper key={idx}>
+                <ChildText style={{ width: "50%" }}>{item.title}</ChildText>
+                <ChildText style={{ width: "25%" }}>{item.author}</ChildText>
+                <ChildText style={{ width: "25%" }}>{item.time}</ChildText>
+              </ChildWrapper>
+            ))}
+          </BoveSmallCon>
         </MoveCon>
-        <FreeCon>
-         
-        </FreeCon>
+        <FreeCon></FreeCon>
         <ImgCon></ImgCon>
       </Wrap>
     </Con>

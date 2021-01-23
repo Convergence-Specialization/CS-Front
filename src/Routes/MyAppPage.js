0302c6a7929e5d9bac64de771dd550a5c2158be5
7dngur7 <7dngur7@naver.com>
2021-01-24 05:15:13 +0900

    나타나게

--- src/Routes/MyAppPage.js
+++ src/Routes/MyAppPage.js
@@ -2,12 +2,26 @@ import Footer from "../Component/Footer";
 import Navbar from "../Component/Navbar";
 import AppPage from "../Component/AppPage";
 import BottomNav from "../Component/BottomNav";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {dbService, firestoreInstance} from "../fbase";
 
 
 const MyAppPage = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async () => {
+      const dbNweets = await dbService.collection("nweets").get();
+      dbNweets.forEach((document) => {
+        const nweetObject = {
+          ...document.data(),
+          id: document.id,
+        };
+        setNweets((prev) => [nweetObject, ...prev]);
+      });
+    };
+    useEffect(() => {
+      getNweets();
+    }, []);
     const onSubmit = async (event) => {
       event.preventDefault();
       await dbService.collection("nweets").add({
@@ -36,6 +50,13 @@ const MyAppPage = () => {
           />
           <input type="submit" value="저장" />
         </form>
+        <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
       </div>
     );
   };export default MyAppPage;

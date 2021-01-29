05864a32f0329c63486414381d7805853f6850c3
Poxios <poxios0310@gmail.com>
2021-01-29 18:25:07 +0900

    api 1차 완료

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://convergence-ssu.herokuapp.com/",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://convergence-ssu.herokuapp.com/",
+  // baseURL: "http://localhost:5000",
 });
 
 const getBearer = () => `Bearer ${localStorage.getItem("idToken")}`;

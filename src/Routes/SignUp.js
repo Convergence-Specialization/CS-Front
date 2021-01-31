db06b0931a9782c3b48af0a18d2502ff4a00ceb4
kimjunha1231 <74090222+kimjunha1231@users.noreply.github.com>
2021-01-31 23:37:36 +0900

    사인업

--- src/Routes/SignUp.js
+++ src/Routes/SignUp.js
@@ -1,7 +1,13 @@
 import React from "react";
 import SignUp from "../Component/SignUp";
-
-const SignUpPresenter = () => (
-    <SignUp />
-);
+import Navbar from "../Component/Navbar";
+import { mainPageIcons } from "../assets/Resources";
+const SignUpPresenter = () => {
+  return (
+    <>
+      <Navbar isTransparent />
+      <SignUp />
+    </>
+  );
+};
 export default SignUpPresenter;

49f36b8aee3209df69d52204fbb94a955cec17b7
kimjunha1231 <74090222+kimjunha1231@users.noreply.github.com>
2021-02-08 01:13:54 +0900

    NavBot

--- src/Routes/Board/Announcement.js
+++ src/Routes/Board/Announcement.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
-
 import Read from "../../Component/Board/Announcement/Normal/Read";
 import Create from "../../Component/Board/Announcement/Normal/Create";
 import ListView from "../../Component/Board/Announcement/Event/ListView";
 import EventPost from "../../Component/Board/Announcement/Event/EventPost";
-
+import { mainPageIcons } from "../../assets/Resources";
 import { useLocation } from "react-router-dom";
 import Navbar from "../../Component/Navbar";
-
+import NavBot from "../../Component/SmallComponents/NavBot";
 const AnnouncePresenter = () => {
   const location = useLocation();
   const [renderElement, setRenderElement] = useState(<div />);
@@ -32,6 +31,7 @@ const AnnouncePresenter = () => {
   return (
     <>
       <Navbar />
+      <NavBot Icon={mainPageIcons.announce} Name="학부행사" />
       {renderElement}
     </>
   );

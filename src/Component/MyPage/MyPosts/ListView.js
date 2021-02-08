542d5f93b97bd2026d6f279959195a303a97fd44
kimjunha1231 <74090222+kimjunha1231@users.noreply.github.com>
2021-02-08 23:40:50 +0900

    더보기 버튼

--- src/Component/MyPage/MyPosts/ListView.js
+++ src/Component/MyPage/MyPosts/ListView.js
@@ -1,60 +1,77 @@
 import React from "react";
 import styled from "styled-components";
-import NavBot from "../../SmallComponents/NavBot";
 import { Icons } from "../../../assets/Resources";
 
 const Back = styled.div`
-    background-color: #F5F5F5;
-    padding: 10px 0px;
+  background-color: #f5f5f5;
+  padding: 10px 0px;
 `;
 
 const Board = styled.div`
-    width: 90%;
-    height: 250px;
-    padding: 15px;
-    display: flex;
-    margin: 10px auto;
-    flex-direction: column;
-    border-radius: 15px;
-    box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
-    font-weight: bold;
-    font-size: 17px;
-    background-color: white;
+  width: 90%;
+  height: 250px;
+  padding: 5px 15px;
+  display: flex;
+  margin: 10px auto;
+  flex-direction: column;
+  border-radius: 15px;
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+  background-color: white;
 `;
 
 const Box = styled.div`
-    padding: 0px 0px 10px 0px;
-    border-bottom: 2px solid #aca9a9;
+  border-bottom: 2px solid #aca9a9;
+  display: flex;
+  padding: 5px 0px;
+  align-items: center;
+  justify-content: space-between;
 `;
-
+const Text = styled.div`
+  font-weight: bold;
+  font-size: 17px;
+`;
+const Button = styled.div`
+  text-align: center;
+  padding: 6px 14px;
+  border-radius: 20px;
+  font-size: 13px;
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+  background-color: #d4e6fb;
+  cursor: pointer;
+  @media (max-width: 430px) {
+    font-size: 12px;
+  }
+`;
 const BlankPost = styled.div`
-    padding: 70px 10px 10px 10px;
-    margin: 10px auto;
-    font-weight: 600;
-    font-size: 14px;
-    text-align: center;
-    color: #C8C8C8;
+  padding: 70px 10px 10px 10px;
+  margin: 10px auto;
+  font-weight: 600;
+  font-size: 14px;
+  text-align: center;
+  color: #c8c8c8;
 `;
 
 const MyPost = () => {
-    return (
-        <>
-            <NavBot
-                Name="내가 쓴 글"
-                Icon={Icons.check}
-            />
-            <Back>
-                <Board>
-                    <Box>융특게시판</Box>
-                    <BlankPost>⁕작성한 내용이 없습니다⁕</BlankPost>
-                </Board> 
-                <Board>
-                    <Box>전과게시판</Box>
-                    <BlankPost>⁕작성한 내용이 없습니다⁕</BlankPost>
-                </Board>
-            </Back>
-        </>
-    );
-}
+  return (
+    <>
+      <Back>
+        <Board>
+          <Box>
+            <Text>융특 게시판</Text>
+            <Button>더보기</Button>
+          </Box>
+          <BlankPost>⁕작성한 내용이 없습니다⁕</BlankPost>
+        </Board>
+        <Board>
+          <Box>
+            <Text>전과 게시판</Text>
+            <Button>더보기</Button>
+          </Box>
+          <BlankPost>⁕작성한 내용이 없습니다⁕</BlankPost>
+        </Board>
+      </Back>
+    </>
+  );
+};
 
 export default MyPost;
--- src/Routes/MyPage/MyPostsListView.js
+++ src/Routes/MyPage/MyPostsListView.js
@@ -1,10 +1,12 @@
 import Navbar from "../../Component/Navbar";
 import MyPosts from "../../Component/MyPage/MyPosts/ListView"; //마이포스츠만 써줄라면 인데스js쓰기
-
+import NavBot from "../../Component/SmallComponents/NavBot";
+import { Icons } from "../../assets/Resources";
 export default () => {
   return (
     <>
       <Navbar Navname="마이페이지" />
+      <NavBot Name="내가 쓴 글" Icon={Icons.check} />
       <MyPosts />
     </>
   );

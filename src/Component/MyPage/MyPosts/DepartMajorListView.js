d226d1f8fb27dcc5920b951a9ea30043f8b80456
kimjunha1231 <74090222+kimjunha1231@users.noreply.github.com>
2021-02-09 02:04:19 +0900

    내가 쓴 글 끝

--- src/Component/MyPage/MyPosts/DepartMajorListView.js
+++ src/Component/MyPage/MyPosts/DepartMajorListView.js
@@ -6,6 +6,7 @@ import { message } from "antd";
 import { departMajorApi } from "../../../api";
 import { subjectDicts } from "../../../assets/Dicts";
 
+let a= 5;
 const Box = styled.div`
   border-bottom: 2px solid #aca9a9;
   display: flex;
@@ -94,13 +95,23 @@ const BlankPost = styled.div`
   text-align: center;
   color: #c8c8c8;
 `;
-const MyPost = () => {
+const MoreButton = styled.div`
+  width: 95%;
+  margin: 0 auto;
+  text-align: center;
+  font-weight: bold;
+  border-radius: 15px;
+  padding: 10px 0;
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+  background-color: white;
+`;
+const DepartMajorListView = () => {
   const history = useHistory();
   const [posts, setPosts] = useState([]);
   useEffect(() => {
     // TODO: API에서 더보기 구현.
     departMajorApi
-      .getLists({ size: 3 })
+      .getLists({ size: 7 })
       .then((docsArray) => setPosts(docsArray))
       .catch((error) => message.error(error.message));
   }, []);
@@ -108,85 +119,7 @@ const MyPost = () => {
     <>
       <BoardContainer>
         <Box>
-          <Text >
-              융특 게시판</Text>
-          <Button
-           onClick={() => {
-            history.push({
-              pathname: `myposts`,
-              state: { pageName: "convergencelistview" },
-            });
-          }}
-          >더보기</Button>
-        </Box>
-        {posts.length === 0 ? (
-          <BlankPost>⁕작성한 내용이 없습니다⁕</BlankPost>
-        ) : (
-          posts.map((item, idx) => (
-            <BoardChildWrapper
-              key={idx}
-              onClick={() =>
-                history.push({
-                  pathname: `/board/departmajor`,
-                  state: {
-                    pageName: "read",
-                    docItem: item,
-                  },
-                })
-              }
-            >
-              <BoardChildTitleWrapper>
-                {item.subject !== "NONE" && (
-                  <SubjectSelectImg
-                    style={{ width: "23px", marginTop: "-5px" }}
-                    src={subjectDicts[item.subject].img}
-                    alt={"asdf"}
-                  />
-                )}
-                {item.subject === "NONE" && (
-                  <BoardChildTitle style={{ width: "80%" }}>
-                    {item.title}
-                  </BoardChildTitle>
-                )}
-                {item.subject !== "NONE" && (
-                  <BoardChildTitle style={{ width: "72%" }}>
-                    {item.title}
-                  </BoardChildTitle>
-                )}
-              </BoardChildTitleWrapper>
-              <BoardChildContent>{item.content}</BoardChildContent>
-              <BoardChildTimeText>
-                {item.timestampDistance} 전
-              </BoardChildTimeText>
-              <BoardChildMetaText>
-                <img
-                  src={mainPageIcons.heart}
-                  alt="하트 아이콘"
-                  style={{ width: "15px", marginRight: " 2px" }}
-                />
-                <div>{item.commentCount}</div>
-                <img
-                  src={readDoc.speech_bubble}
-                  alt="말풍선 아이콘"
-                  style={{ width: "15px", margin: "0px 4px" }}
-                />
-                <div>{item.likeCount}</div>
-              </BoardChildMetaText>
-            </BoardChildWrapper>
-          ))
-        )}
-      </BoardContainer>
-      <BoardContainer>
-        <Box>
           <Text>전과 게시판</Text>
-          <Button
-           onClick={() => {
-            history.push({
-              pathname: `myposts`,
-              state: { pageName: "departmajorlistview" },
-            });
-          }}
-          >더보기</Button>
         </Box>
         {posts.length === 0 ? (
           <BlankPost>⁕작성한 내용이 없습니다⁕</BlankPost>
@@ -244,9 +177,20 @@ const MyPost = () => {
             </BoardChildWrapper>
           ))
         )}
+        
       </BoardContainer>
+      <MoreButton
+          onClick={() => {
+            departMajorApi
+              .getLists({ size: 10 + a })
+              .then((docsArray) => setPosts(docsArray))
+              .catch((error) => message.error(error.message));
+            a = a + 5;
+          }}>
+          더보기
+        </MoreButton>
     </>
   );
 };
 
-export default MyPost;
+export default DepartMajorListView;
--- src/Routes/MyPage/MyPostsListView.js
+++ src/Routes/MyPage/MyPostsListView.js
@@ -1,13 +1,34 @@
 import Navbar from "../../Component/Navbar";
-import MyPosts from "../../Component/MyPage/MyPosts/ListView"; //마이포스츠만 써줄라면 인데스js쓰기
+import MyPosts from "../../Component/MyPage/MyPosts/ListView";
+import ConvergenceListView from "../../Component/MyPage/MyPosts/ConvergenceListView";
+import DepartMajorListView from "../../Component/MyPage/MyPosts/DepartMajorListView";
 import NavBot from "../../Component/SmallComponents/NavBot";
 import { Icons } from "../../assets/Resources";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 export default () => {
+  const location = useLocation();
+  const [renderElement, setRenderElement] = useState(<div />);
+
+  useEffect(() => {
+    switch (!!location.state && location.state.pageName) {
+      case "convergencelistview":
+        setRenderElement(<ConvergenceListView />);
+        break;
+        case "departmajorlistview":
+        setRenderElement(<DepartMajorListView />);
+        break;
+      default:
+        setRenderElement(
+            <MyPosts />
+        );
+    }
+  }, [location]);
   return (
     <>
-      <Navbar Navname="마이페이지" />
+        <Navbar Navname="마이페이지" />
       <NavBot Name="내가 쓴 글" Icon={Icons.check} />
-      <MyPosts />
+      {renderElement}
     </>
   );
 };

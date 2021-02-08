9867e3f523e38d1fc0b66898d3d72ce20619e329
Poxios <poxios0310@gmail.com>
2021-02-08 23:52:51 +0900

    why my token not working

--- src/Component/Watchers.js
+++ src/Component/Watchers.js
@@ -23,6 +23,7 @@ export const UseGoogleAnalytics = () => {
     const idToken = localStorage.getItem("idToken");
     if (idToken !== null) {
       if (jwtDecode(idToken).exp * 1000 < new Date().getTime()) {
+        message.info("토큰 재발급 시도");
         authService.currentUser
           .getIdToken()
           .then((token) => localStorage.setItem("idToken", token))

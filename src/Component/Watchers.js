2cfc3ee4e1d744c8aab5bc184aea7e678a690682
Poxios <poxios0310@gmail.com>
2021-02-11 10:32:23 +0900

    same

--- src/Component/Watchers.js
+++ src/Component/Watchers.js
@@ -4,21 +4,44 @@ import { useLocation } from "react-router-dom";
 import ReactGA from "react-ga";
 import jwtDecode from "jwt-decode";
 import { message } from "antd";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
+const NEED_PERMISSION_PATHS = [
+  "/board/departmajor",
+  "/board/convergence",
+  "/board/announcement",
+];
 
 export const UseGoogleAnalytics = () => {
   const location = useLocation();
+  const history = useHistory();
+
+  const isPageInLoginRequired = (currentPath) => {
+    let isPassed = false;
+    NEED_PERMISSION_PATHS.forEach((path) => {
+      if (path === currentPath) {
+        isPassed = true;
+      }
+    });
+    return isPassed;
+  };
 
   useEffect(() => {
     ReactGA.initialize("UA-189137891-1");
   }, []);
 
   useEffect(() => {
-    console.log("페이지 바뀜");
+    // 로그인 해야하는 페이지 접근할 때
+    if (isPageInLoginRequired(location.pathname) && !authService.currentUser) {
+      history.push("/login/loginrequire");
+    }
 
+    // GA 페이지 트레킹
     const currentPath = location.pathname + location.search;
     ReactGA.set({ page: currentPath });
     ReactGA.pageview(currentPath);
 
+    // idToken 만료 확인 후 재발급.
     const idToken = localStorage.getItem("idToken");
     if (idToken !== null && idToken !== "") {
       if (jwtDecode(idToken).exp * 1000 < new Date().getTime()) {

a89615fd75c393167e36dcdc74ca8e2897560607
Poxios <poxios0310@gmail.com>
2021-02-11 10:32:15 +0900

    check auth & refresh token

--- src/Component/Router.js
+++ src/Component/Router.js
@@ -36,85 +36,83 @@ import { UseGoogleAnalytics } from "./Watchers";
 const RouterComponent = () => {
   UseGoogleAnalytics();
   return (
-    <Router basename="/CS-Front">
-      <Switch>
-        <Route path="/" exact>
-          <MainPage />
-        </Route>
-        <Route path="/login" exact>
-          <LoginPage />
-        </Route>
-        <Route path="/login/lostpw">
-          <ForgotPassword />
-        </Route>
-        <Route path="/login/noinformation">
-          <NoInformation />
-        </Route>
-        <Route path="/login/addinformation">
-          <AddInformation />
-        </Route>
-        <Route path="/login/loginrequire">
-          <LoginRequire />
-        </Route>
-        <Route path="/signup" exact>
-          <SignUp />
-        </Route>
-        <Route path="/notification" exact>
-          <Notification />
-        </Route>
-        <Route path="/signup/google">
-          <SignUpGoogle />
-        </Route>
-        <Route path="/board/convergence">
-          <Convergence />
-        </Route>
-        <Route path="/board/event/eventpost">
-          <EventPost />
-        </Route>
-        <Route path="/board/majorintroduce">
-          <MajorIntroduce />
-        </Route>
-        <Route path="/board/event/normalcreate">
-          <NormalCreate />
-        </Route>
-        <Route path="/board/departmajor">
-          <DepartMajor />
-        </Route>
-        <Route path="/board/event" exact>
-          <Event />
-        </Route>
-        <Route path="/board/announcement" exact>
-          <Announcement />
-        </Route>
-        <Route path="/mypage" exact>
-          <MyPage />
-        </Route>
-        <Route path="/mypage/myposts">
-          <MyPosts />
-        </Route>
-        <Route path="/mypage/changeinformation" exact>
-          <ChangeInformation />
-        </Route>
-        {/* TODO: 얘네는 글쓰는 페이지라서 위에 라우트 연결 하면 됨! */}
-        <Route path="/mypage/changepw" exact>
-          <ChangePW />
-        </Route>
-        <Route path="/IconsSource" exact>
-          <IconsSource />
-        </Route>
-        <Route path="/makers" exact>
-          <Makers />
-        </Route>
-        <Route path="/suggestions" exact>
-          <Suggestions />
-        </Route>
-        <Redirect path="*" to="/" />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route path="/" exact>
+        <MainPage />
+      </Route>
+      <Route path="/login" exact>
+        <LoginPage />
+      </Route>
+      <Route path="/login/lostpw">
+        <ForgotPassword />
+      </Route>
+      <Route path="/login/noinformation">
+        <NoInformation />
+      </Route>
+      <Route path="/login/addinformation">
+        <AddInformation />
+      </Route>
+      <Route path="/login/loginrequire">
+        <LoginRequire />
+      </Route>
+      <Route path="/signup" exact>
+        <SignUp />
+      </Route>
+      <Route path="/notification" exact>
+        <Notification />
+      </Route>
+      <Route path="/signup/google">
+        <SignUpGoogle />
+      </Route>
+      <Route path="/board/convergence">
+        <Convergence />
+      </Route>
+      <Route path="/board/event/eventpost">
+        <EventPost />
+      </Route>
+      <Route path="/board/majorintroduce">
+        <MajorIntroduce />
+      </Route>
+      <Route path="/board/event/normalcreate">
+        <NormalCreate />
+      </Route>
+      <Route path="/board/departmajor">
+        <DepartMajor />
+      </Route>
+      <Route path="/board/event" exact>
+        <Event />
+      </Route>
+      <Route path="/board/announcement" exact>
+        <Announcement />
+      </Route>
+      <Route path="/mypage" exact>
+        <MyPage />
+      </Route>
+      <Route path="/mypage/myposts">
+        <MyPosts />
+      </Route>
+      <Route path="/mypage/changeinformation" exact>
+        <ChangeInformation />
+      </Route>
+      {/* TODO: 얘네는 글쓰는 페이지라서 위에 라우트 연결 하면 됨! */}
+      <Route path="/mypage/changepw" exact>
+        <ChangePW />
+      </Route>
+      <Route path="/IconsSource" exact>
+        <IconsSource />
+      </Route>
+      <Route path="/makers" exact>
+        <Makers />
+      </Route>
+      <Route path="/suggestions" exact>
+        <Suggestions />
+      </Route>
+      <Redirect path="*" to="/" />
+    </Switch>
   );
 };
 const RouterExporter = () => (
-  <Router>
+  <Router basename="/CS-Front">
     <RouterComponent />
   </Router>
 );

7ecf496b74d5c263f3b9528a62b82a8d7792c9c7
ywoo121 <ywoo121@naver.com>
2021-02-13 00:43:53 +0900

    팀소개 페이지 footer추가

--- src/assets/Resources.js
+++ src/assets/Resources.js
@@ -15,7 +15,7 @@ export const Icons = {
   UpArrow:
     "https://user-images.githubusercontent.com/74090222/104585329-31cd5700-56a7-11eb-913d-0daddf15de16.png",
   숭실:
-    "https://user-images.githubusercontent.com/74090222/104586083-5413a480-56a8-11eb-865d-e154a8f65127.png",
+    "https://user-images.githubusercontent.com/71367047/107774976-14150f80-6d83-11eb-91ca-e15bb9de74ee.png",
   Home:
     "https://user-images.githubusercontent.com/74090222/106783350-8d7b7680-668e-11eb-881e-4a78ff36388c.png",
   menu:

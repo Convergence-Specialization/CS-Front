57918b45c8b806870a76defd477991088798b1c3
Poxios <poxios0310@gmail.com>
2021-02-14 18:34:01 +0900

    serviceworker test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./Component/App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import "antd/dist/antd.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+serviceWorkerRegistration.register();

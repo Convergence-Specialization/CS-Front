e7c58142d5cff4b32043cd102a19d1c6ff9c0e99
ywoo121 <ywoo121@users.noreply.github.com>
2021-02-14 15:12:04 +0000

    deploy: 620611ef297a069ba94a09d306c2c373c40b94d8

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 self.__precacheManifest = [
   {
     "url": "asset-manifest.json",
-    "revision": "3017ec45ba7ac92f88075da713352f93"
+    "revision": "c2a5f83b3d474bcf05e8a92a287c64bb"
   },
   {
     "url": "favicon.ico",
@@ -60,8 +60,8 @@ self.__precacheManifest = [
     "revision": "0dc04204e9b507a7e6aea2320b51283f"
   },
   {
-    "url": "static/js/main.8b5fdaf9.chunk.js",
-    "revision": "1474fe331eef71a08b63eed81c41df49"
+    "url": "static/js/main.4dec0bda.chunk.js",
+    "revision": "acf342f2f12b27ac9aebf1293546364d"
   },
   {
     "url": "static/js/runtime-main.aab4bf4c.js",

26c244eb27cf6617348bdfac650866feb6aae133
eunsoA <eunsoA@users.noreply.github.com>
2021-02-15 15:18:25 +0000

    deploy: 59b36d393d5484ddb93d0f9f8be88c2f66cf20c7

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 self.__precacheManifest = [
   {
     "url": "asset-manifest.json",
-    "revision": "85720d3b234752d94cbfccd9d237055d"
+    "revision": "412de69050e11cc1b84be786292fcdf9"
   },
   {
     "url": "favicon.ico",
@@ -60,8 +60,8 @@ self.__precacheManifest = [
     "revision": "0dc04204e9b507a7e6aea2320b51283f"
   },
   {
-    "url": "static/js/main.3fabf5c7.chunk.js",
-    "revision": "c048aabb91c5c000cfa768cb33e1ed48"
+    "url": "static/js/main.4f92a6ed.chunk.js",
+    "revision": "1695494d4580830eb7537290742aae53"
   },
   {
     "url": "static/js/runtime-main.aab4bf4c.js",

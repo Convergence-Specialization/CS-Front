30fc87aec523da46f8fc53f733ac017ba698abbe
ywoo121 <ywoo121@naver.com>
2021-02-19 03:16:26 +0900

    동그란 말 추가, 로그인 화면 이미지 변경

--- src/assets/Resources.js
+++ src/assets/Resources.js
@@ -15,6 +15,7 @@ import home from "../assets/sideBarIcons/home.png";
 import downButton from "../assets/sideBarIcons/downButton.png";
 import upButton from "../assets/sideBarIcons/upButton.png";
 import newhorse from "../assets/horseIcons/newhorse.png";
+import horse from "../assets/horseIcons/horse.png";
 import face from "../assets/horseIcons/face.png";
 import announce from "../assets/navbotIcons/announce.png";
 import airplane from "../assets/navbotIcons/airplane.png";
@@ -97,6 +98,7 @@ export const navIcons = {
 export const horseIcons = {
   newhorse,
   face,
+  horse,
 };
 export const navbotIcons = {
   announce,

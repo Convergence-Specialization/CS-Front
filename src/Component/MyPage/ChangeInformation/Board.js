9c30322fd9597d4222c0d6dad0ef658456aaf110
YoungHyun <poxios0310@gmail.com>
2021-02-19 19:30:00 +0900

    temp

--- src/Component/MyPage/ChangeInformation/Board.js
+++ src/Component/MyPage/ChangeInformation/Board.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { useAuth } from "../../Watchers";
+import { loginFunctions, useAuth } from "../../Watchers";
 import message from "antd/lib/message";
 import SelectSubjectModal from "../Modal";
+import LoadingComponent from "../../SmallComponents/Loading";
+import { userApi } from "../../../api";
 
 const Container = styled.div`
   padding: 20px;
@@ -25,51 +27,77 @@ const Title = styled.div`
 const Text = styled.div`
   font-size: 15px;
 `;
+const DataInputBox = styled.input`
+  width: 100%;
+  outline: none;
+  border: none;
+`;
 
 const Board = () => {
-  const user = useAuth();
   const [subjectModalVisible, setSubjectModalVisible] = useState(false);
+  const [myInfo, setMyInfo] = useState(null);
+  useEffect(() => {
+    const userInfo = loginFunctions.getUserInfo();
+    if (!userInfo) return;
+    userApi
+      .getMyInfo({ uid: userInfo.uid })
+      .then((info) => setMyInfo(info))
+      .catch((error) => console.log(error));
+  }, []);
   return (
     <>
-      <SelectSubjectModal
-        visible={subjectModalVisible}
-        onClose={() => setSubjectModalVisible(false)}
-        name="회원 정보가 수정되었습니다."
-      />
-      <Title style={{ marginTop: "40px" }}>이름</Title>
-      <Container>
-        <Text>
-          {!!user
-            ? (!!user.displayName ? user.displayName : "융슝이").concat()
-            : ""}
-        </Text>
-      </Container>
-      <Title>학번</Title>
-      <Container>
-        <Text>20201884</Text>
-      </Container>
-      <Title>이메일</Title>
-      <Container style={{ marginBottom: "0px" }}>
-        <Text>
-          {!!user ? (!!user.email ? user.email : "융슝이").concat() : ""}
-        </Text>
-      </Container>
-      <Text style={{ marginTop: "13px", marginLeft: "40px" }}>
-        ※ 이메일은 수정되지 않습니다.
-      </Text>
-      <Container
-        onClick={() => {
-          message.success("정보가 수정되었습니다.");
-          setSubjectModalVisible(true);
-        }}
-        style={{ backgroundColor: "lightgray", marginTop: "35px" }}
-      >
-        <Text
-          style={{ textAlign: "center", fontSize: "16px", fontWeight: "bold" }}
-        >
-          정보 수정 완료
-        </Text>
-      </Container>
+      {!myInfo ? (
+        <LoadingComponent />
+      ) : (
+        <>
+          <SelectSubjectModal
+            visible={subjectModalVisible}
+            onClose={() => setSubjectModalVisible(false)}
+            name="회원 정보가 수정되었습니다."
+          />
+          <Title style={{ marginTop: "40px" }}>이름</Title>
+          <Container>
+            <DataInputBox
+              id="nameInput"
+              defaultValue={loginFunctions.getUserInfo().displayName}
+            />
+          </Container>
+          <Title>학번</Title>
+          <Container>
+            <DataInputBox
+              id="studentIdInput"
+              defaultValue={myInfo.student_id}
+            />
+          </Container>
+          <Title>이메일</Title>
+          <Container style={{ marginBottom: "0px" }}>
+            <DataInputBox
+              id="emailInput"
+              disabled={true}
+              defaultValue={loginFunctions.getUserInfo().email}
+            />
+          </Container>
+          <Text style={{ marginTop: "13px", marginLeft: "40px" }}>
+            ※ 이메일은 수정되지 않습니다.
+          </Text>
+          <Container
+            onClick={() => {
+              // message.success("정보가 수정되었습니다.");
+              // setSubjectModalVisible(true);
+              message.info("정보 변경은 건의하기를 사용해주시기 바랍니다.");
+            }}
+            style={{ backgroundColor: "lightgray", marginTop: "35px" }}>
+            <Text
+              style={{
+                textAlign: "center",
+                fontSize: "16px",
+                fontWeight: "bold",
+              }}>
+              정보 수정 완료
+            </Text>
+          </Container>
+        </>
+      )}
     </>
   );
 };
--- src/Component/MyPage/ChangeInformation/index.js
+++ src/Component/MyPage/ChangeInformation/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Board from "./Board";
 
-
 const ChangeInformation =() => {
-  // const classes = useStyles();
   return (
     <div>
       <Board />

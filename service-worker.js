4c7aa2aa284687c1deb14eb138444bbfb32d4d70
Poxios <Poxios@users.noreply.github.com>
2021-02-21 07:26:43 +0000

    deploy: 5105acd7d59195e370d6b3d6d228ec61fe2e07d7

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 self.__precacheManifest = [
   {
     "url": "asset-manifest.json",
-    "revision": "f41e8c209eaa3b51355990f9b5914e5b"
+    "revision": "054c83e6948462302f6296fce7897f0a"
   },
   {
     "url": "favicon.ico",
@@ -60,8 +60,8 @@ self.__precacheManifest = [
     "revision": "0dc04204e9b507a7e6aea2320b51283f"
   },
   {
-    "url": "static/js/main.f0276262.chunk.js",
-    "revision": "a92b4dc48882353721437fbec149c04a"
+    "url": "static/js/main.45979c4c.chunk.js",
+    "revision": "804d51f8f053d0faca7bd11012a3f63f"
   },
   {
     "url": "static/js/runtime-main.aab4bf4c.js",

ecd2cd08f8034effa9c25ba1b9bbd420668744cd
YoungHyun <poxios0310@gmail.com>
2021-02-27 17:43:47 +0900

    test

--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,8 @@ import "firebase/firestore";
 import "firebase/auth";
 import "firebase/storage";
 
+console.log(process.env)
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,

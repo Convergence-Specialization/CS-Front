1f49e83c6e59294bcff0158b1a7663dc2dfd3b15
kimjunha1231 <kimjunha1231@users.noreply.github.com>
2021-03-05 07:14:37 +0000

    deploy: e239ded7f5a09ab2ce37c5a50e340cb4c27f782d

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 self.__precacheManifest = [
   {
     "url": "asset-manifest.json",
-    "revision": "c72ab02b2703391be98b5d874644895c"
+    "revision": "e0bc1f2d77e1cc70e8a5d513db50acd0"
   },
   {
     "url": "favicon.ico",
@@ -60,8 +60,8 @@ self.__precacheManifest = [
     "revision": "744e1605270ce0f20ba5f3ea8ae6a094"
   },
   {
-    "url": "static/js/main.1a275fab.chunk.js",
-    "revision": "9954a2d58057cccb703871099dc727f7"
+    "url": "static/js/main.1a14e81c.chunk.js",
+    "revision": "c96a71b2f2e89fb367064cf519240346"
   },
   {
     "url": "static/js/runtime-main.c86d2cdf.js",
